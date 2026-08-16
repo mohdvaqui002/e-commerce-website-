f02f922439907edef3a92dc62afa6fab446e3616
Mohd Vaqui <mohdvaqui002@gmail.com>
2026-08-17 01:38:25 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./Components/Navbar/Navbar"
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {HashRouter, Route, Routes} from "react-router-dom"
 import Shop from "./Pages/Shop"
 import ShopCategory from "./Pages/ShopCategory"
 import Product from "./Pages/Product"
@@ -13,7 +13,7 @@ function App() {
  
   return (
     <div >
-      <BrowserRouter>
+      <HashRouter>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Shop/>}/>
@@ -26,7 +26,7 @@ function App() {
         <Route path="/login" element={<LoginSignup/>}/>
       </Routes>
       <Footer/>
-      </BrowserRouter>
+      </HashRouter>
 
     </div>
      
